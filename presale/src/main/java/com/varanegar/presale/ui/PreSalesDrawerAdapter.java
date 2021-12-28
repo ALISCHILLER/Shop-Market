7ff7a50a62d7ad80901ddb5fa47c2ec7422c6a76
Ali Soleimani <solimaniali90@gmail.com>
2021-12-28 16:45:33 +0330

    release/Presale-3.1.1.148

--- presale/src/main/java/com/varanegar/presale/ui/PreSalesDrawerAdapter.java
+++ presale/src/main/java/com/varanegar/presale/ui/PreSalesDrawerAdapter.java
@@ -20,7 +20,7 @@ public class PreSalesDrawerAdapter extends MainDrawerAdapter {
         /**
          * گزارشات قبلی
          */
-        //        add(1, new PreSalesTourReportDrawerItem(activity));
+                add(1, new PreSalesTourReportDrawerItem(activity));
 //        reports.addItem(new DrawerItem(activity, R.string.inventory_qty).setClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
