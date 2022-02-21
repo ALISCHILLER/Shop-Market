153c949c2158e8cf91450c43a4b51b72a3320434
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-02-21 18:42:26 +0330

    news supervisor

--- supervisor/src/main/java/com/varanegar/supervisor/news_fragment/News_Adapter.java
+++ supervisor/src/main/java/com/varanegar/supervisor/news_fragment/News_Adapter.java
@@ -1,2 +1,46 @@
-package com.varanegar.supervisor.news_fragment;public class News_Adapter {
+package com.varanegar.supervisor.news_fragment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.varanegar.supervisor.R;
+
+public class News_Adapter extends
+        RecyclerView.Adapter<News_Adapter.ViewHolder> {
+
+    private  Context mcontext;
+
+    public News_Adapter(Context context){
+        this.mcontext=context;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View view=inflater.inflate(R.layout.item_list_news,parent,false);
+        ViewHolder viewHolder=new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
