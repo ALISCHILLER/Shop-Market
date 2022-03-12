82ffc35f61cbe5b931b17f2d99fbdca3da6ff64d
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-03-12 17:10:29 +0330

    slider news

--- supervisor/src/main/java/com/varanegar/supervisor/fragment/news_fragment/News_Fragment.java
+++ supervisor/src/main/java/com/varanegar/supervisor/fragment/news_fragment/News_Fragment.java
@@ -1,22 +1,51 @@
 package com.varanegar.supervisor.fragment.news_fragment;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StyleRes;
+import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.varanegar.framework.ui.card_slider.CardSliderLayoutManager;
+import com.varanegar.framework.ui.card_slider.CardSnapHelper;
 import com.varanegar.supervisor.IMainPageFragment;
 import com.varanegar.supervisor.R;
 import com.varanegar.supervisor.fragment.news_fragment.cards.SliderAdapter;
 
 public class News_Fragment extends IMainPageFragment {
     private RecyclerView recyclerView;
-    private final int[] pics = {R.drawable.zar};
+
     private  SliderAdapter sliderAdapter;
+    private CardSliderLayoutManager layoutManger;
+    private int currentPosition;
+    private TextSwitcher temperatureSwitcher;
+    private TextSwitcher placeSwitcher;
+    private TextSwitcher clockSwitcher;
+    private TextSwitcher descriptionsSwitcher;
+    private View greenDot;
+    private final int[] pics = {R.drawable.zar};
+    private final int[][] dotCoords = new int[5][2];
+    private final int[] descriptions = {R.string.text1
+            ,R.string.text2,
+            R.string.text3,
+            R.string.text4,
+            R.string.text5};
+    private final String[] countries = {"PARIS", "SEOUL", "LONDON", "BEIJING", "THIRA"};
+    private final String[] places = {"The Louvre", "Gwanghwamun", "Tower Bridge", "Temple of Heaven"
+            , "Aegeana Sea"};
+    private final String[] temperatures = {"21°C", "19°C", "17°C", "23°C", "20°C"};
+    private final String[] times = {"Aug 1 - Dec 15    7:00-18:00", "Sep 5 - Nov 10    8:00-16:00"
+            , "Mar 8 - May 21    7:00-18:00"};
     @Override
     protected View onCreateContentView(@NonNull LayoutInflater inflater,@Nullable ViewGroup container
             ,@Nullable Bundle savedInstanceState) {
@@ -31,8 +60,120 @@ public class News_Fragment extends IMainPageFragment {
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
         recyclerView.setAdapter(sliderAdapter);
         recyclerView.setHasFixedSize(true);
+
+        temperatureSwitcher = (TextSwitcher)view.findViewById(R.id.ts_temperature);
+        temperatureSwitcher.setFactory(new TextViewFactory(R.style.TemperatureTextView, true));
+        temperatureSwitcher.setCurrentText(temperatures[0]);
+        placeSwitcher = (TextSwitcher) view.findViewById(R.id.ts_place);
+        placeSwitcher.setFactory(new TextViewFactory(R.style.PlaceTextView, false));
+        placeSwitcher.setCurrentText(places[0]);
+
+        clockSwitcher = (TextSwitcher) view.findViewById(R.id.ts_clock);
+        clockSwitcher.setFactory(new TextViewFactory(R.style.ClockTextView, false));
+        clockSwitcher.setCurrentText(times[0]);
+        descriptionsSwitcher = (TextSwitcher) view.findViewById(R.id.ts_description);
+        descriptionsSwitcher.setInAnimation(getActivity(), android.R.anim.fade_in);
+        descriptionsSwitcher.setOutAnimation(getActivity(), android.R.anim.fade_out);
+        descriptionsSwitcher.setFactory(new TextViewFactory(R.style.DescriptionTextView, false));
+        descriptionsSwitcher.setCurrentText(getString(descriptions[0]));
+
+
+    }
+    private void initRecyclerView() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    onActiveCardChange();
+                }
+            }
+        });
+
+        layoutManger = (CardSliderLayoutManager) recyclerView.getLayoutManager();
+
+        new CardSnapHelper().attachToRecyclerView(recyclerView);
     }
 
+    private void onActiveCardChange() {
+        final int pos = layoutManger.getActiveCardPosition();
+        if (pos == RecyclerView.NO_POSITION || pos == currentPosition) {
+            return;
+        }
+
+        onActiveCardChange(pos);
+    }
+
+
+    private void onActiveCardChange(int pos) {
+        int animH[] = new int[] {R.anim.slide_in_right, R.anim.slide_out_left};
+        int animV[] = new int[] {R.anim.slide_in_top, R.anim.slide_out_bottom};
+
+        final boolean left2right = pos < currentPosition;
+        if (left2right) {
+            animH[0] = R.anim.slide_in_left;
+            animH[1] = R.anim.slide_out_right;
+
+            animV[0] = R.anim.slide_in_bottom;
+            animV[1] = R.anim.slide_out_top;
+        }
+
+        setCountryText(countries[pos % countries.length], left2right);
+
+        temperatureSwitcher.setInAnimation(getActivity(), animH[0]);
+        temperatureSwitcher.setOutAnimation(getActivity(), animH[1]);
+        temperatureSwitcher.setText(temperatures[pos % temperatures.length]);
+
+        placeSwitcher.setInAnimation(getActivity(), animV[0]);
+        placeSwitcher.setOutAnimation(getActivity(), animV[1]);
+        placeSwitcher.setText(places[pos % places.length]);
+
+        clockSwitcher.setInAnimation(getActivity(), animV[0]);
+        clockSwitcher.setOutAnimation(getActivity(), animV[1]);
+        clockSwitcher.setText(times[pos % times.length]);
+
+        descriptionsSwitcher.setText(getString(descriptions[pos % descriptions.length]));
+
+
+        ViewCompat.animate(greenDot)
+                .translationX(dotCoords[pos % dotCoords.length][0])
+                .translationY(dotCoords[pos % dotCoords.length][1])
+                .start();
+
+        currentPosition = pos;
+    }
+
+    private class TextViewFactory implements  ViewSwitcher.ViewFactory {
+
+        @StyleRes
+        final int styleId;
+        final boolean center;
+
+        TextViewFactory(@StyleRes int styleId, boolean center) {
+            this.styleId = styleId;
+            this.center = center;
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public View makeView() {
+            final TextView textView = new TextView(getActivity());
+
+            if (center) {
+                textView.setGravity(Gravity.CENTER);
+            }
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                textView.setTextAppearance(getActivity(), styleId);
+            } else {
+                textView.setTextAppearance(styleId);
+            }
+
+            return textView;
+        }
+
+    }
+
+    
     private class OnCardClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
