d6b49b1c49ce37d1c2dda25edb670c3b447a501e
m-latifi <m-latifi@zarmacaron.com>
2022-08-29 09:30:45 +0430

    نمایش ندادن نام پرسنل

--- vaslibrary/src/main/java/com/varanegar/vaslibrary/ui/report/report_new/customer_inventory_report/model/CustomerInventoryReportAdapter.java
+++ vaslibrary/src/main/java/com/varanegar/vaslibrary/ui/report/report_new/customer_inventory_report/model/CustomerInventoryReportAdapter.java
@@ -19,18 +19,20 @@ public class CustomerInventoryReportAdapter extends SimpleReportAdapter<PCustome
 
     @Override
     public void bind(ReportColumns columns, PCustomerInventoryReportModel entity){
-        columns.add(bind(entity, CustomerInventoryReportView.customerCode,"کد مشتری").setWeight(0.8f));
-        columns.add(bind(entity, CustomerInventoryReportView.customerName,"نام مشتری").setWeight(1.5f));
-        columns.add(bind(entity, CustomerInventoryReportView.saleDate,"تاریخ فروش").setWeight(0.8f));
-        columns.add(bind(entity, CustomerInventoryReportView.productCode,"کد کالا").setWeight(0.7f));
+        columns.add(bind(entity, CustomerInventoryReportView.saleDate,"تاریخ فروش").setWeight(1f));
+        columns.add(bind(entity, CustomerInventoryReportView.productCode,"کد کالا").setWeight(1f));
         columns.add(bind(entity, CustomerInventoryReportView.productName,"نام کالا").setWeight(1.5f));
-        columns.add(bind(entity, CustomerInventoryReportView.productGroupName,"گروه کالا").setWeight(0.7f));
-        columns.add(bind(entity, CustomerInventoryReportView.inventoryCustomer,"موجودی مشتری").setWeight(0.8f));
-        columns.add(bind(entity, CustomerInventoryReportView.tedadKochektarinVahed,"تعداد کوچکترین واحد").setWeight(1));
-        columns.add(bind(entity, CustomerInventoryReportView.productionDate,"تاریخ تولید").setWeight(0.8f));
-//        columns.add(bind(entity, CustomerInventoryReportView.personnelName,"نام پرسنل").setWeight(1.5f));
+        columns.add(bind(entity, CustomerInventoryReportView.productGroupName,"گروه کالا").setWeight(1f));
+        columns.add(bind(entity, CustomerInventoryReportView.inventoryCustomer,"موجودی مشتری").setWeight(1f));
+        columns.add(bind(entity, CustomerInventoryReportView.tedadKochektarinVahed,"تعداد کوچکترین واحد").setWeight(2));
+        columns.add(bind(entity, CustomerInventoryReportView.productionDate,"تاریخ تولید").setWeight(1f));
+        columns.add(bind(entity, CustomerInventoryReportView.customerCode,"کد مشتری").setWeight(1f));
+        columns.add(bind(entity, CustomerInventoryReportView.customerName,"نام مشتری").setWeight(1.5f));
         columns.add(bind(entity, CustomerInventoryReportView.pathTitle,"عنوان مسیر").setWeight(1f));
         columns.add(bind(entity, CustomerInventoryReportView.delear,"نام فروشنده").setWeight(1.5f));
+
+
+//        columns.add(bind(entity, CustomerInventoryReportView.personnelName,"نام پرسنل").setWeight(1.5f));
 
 //        columns.add(bind(entity, CustomerInventoryReportView.productGroupNameID,"نام مشتری").setWeight(2));
     }
