6c79889b3c19da2f7904b66bb3531fef012f8d64
m-latifi <m6.latifi@gmail.com>
2022-10-22 16:08:01 +0330

    سرچ پین کد

--- supervisor/src/main/java/com/varanegar/supervisor/fragment/list_pincode_Fragment/ListPinCode_Fragment.java
+++ supervisor/src/main/java/com/varanegar/supervisor/fragment/list_pincode_Fragment/ListPinCode_Fragment.java
@@ -3,9 +3,12 @@ package com.varanegar.supervisor.fragment.list_pincode_Fragment;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,80 +17,135 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.varanegar.framework.database.querybuilder.Query;
+import com.varanegar.framework.database.querybuilder.criteria.Criteria;
 import com.varanegar.framework.util.component.cutemessagedialog.CuteMessageDialog;
 import com.varanegar.framework.util.component.cutemessagedialog.Icon;
 import com.varanegar.supervisor.DataManager;
 import com.varanegar.supervisor.IMainPageFragment;
 import com.varanegar.supervisor.R;
-import com.varanegar.supervisor.firebase.notification.model.PinRequest_;
-import com.varanegar.supervisor.firebase.notification.model.PinRequest_ModelRepository;
 import com.varanegar.supervisor.webapi.model_new.datamanager.CustomerPin;
 import com.varanegar.supervisor.webapi.model_new.datamanager.CustomerPinModel;
 import com.varanegar.supervisor.webapi.model_new.datamanager.CustomerPinModelRepository;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class ListPinCode_Fragment extends IMainPageFragment {
-    private RecyclerView list_request;
+
+    private RecyclerView recyclerViewPins;
     private FloatingActionButton fab;
     private List<CustomerPinModel> customerPinModel;
-    private ListPinCodeAdapter  listPinCodeAdapter;
+    private EditText editTextSearch;
 
+
+    //---------------------------------------------------------------------------------------------- onCreateContentView
     @Override
     protected View onCreateContentView(@NonNull LayoutInflater inflater,
                                        @Nullable ViewGroup container,
                                        @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_list_pin_code_layout
-                ,container,false);
-        list_request=view.findViewById(R.id.recycler_list_request);
-        fab=view.findViewById(R.id.fab);
-        return view;
+        return inflater.inflate(R.layout.fragment_list_pin_code_layout, container, false);
     }
+    //---------------------------------------------------------------------------------------------- onCreateContentView
 
+
+    //---------------------------------------------------------------------------------------------- onViewCreated
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getDataPinRequest();
-        listsetdata();
-        fab.setOnClickListener(new View.OnClickListener() {
+        init(view);
+        setListeners();
+        getAllPing();
+    }
+    //---------------------------------------------------------------------------------------------- onViewCreated
+
+
+    //---------------------------------------------------------------------------------------------- init
+    private void init(@NonNull View view) {
+        editTextSearch = view.findViewById(R.id.editTextSearch);
+        fab = view.findViewById(R.id.fab);
+        recyclerViewPins = view.findViewById(R.id.recycler_list_request);
+    }
+    //---------------------------------------------------------------------------------------------- init
+
+
+    //---------------------------------------------------------------------------------------------- setListeners
+    private void setListeners() {
+        fab.setOnClickListener(v -> restData());
+        editTextSearch.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                restData();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().isEmpty())
+                    getAllPing();
+                else
+                    getDataPinFilter(s.toString());
             }
         });
     }
-    private void getDataPinRequest(){
-        Date now = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-        String date = simpleDateFormat.format(now);
-        CustomerPinModelRepository pinModelRepository = new CustomerPinModelRepository();
+    //---------------------------------------------------------------------------------------------- setListeners
+
+
+    //---------------------------------------------------------------------------------------------- getDataPinFilter
+    private void getDataPinFilter(String search) {
+        Query q = new Query();
+        q.from(CustomerPin.CustomerPinTbl)
+                .whereOr(Criteria.contains(CustomerPin.CustomerName, search))
+                .whereOr(Criteria.contains(CustomerPin.CustomerCode, search));
+        getDataPinRequest(q);
+    }
+    //---------------------------------------------------------------------------------------------- getDataPinFilter
+
+
+    //---------------------------------------------------------------------------------------------- getAllPing
+    private void getAllPing() {
         Query q = new Query();
         q.from(CustomerPin.CustomerPinTbl);
-        customerPinModel=pinModelRepository.getItems(q);
+        getDataPinRequest(q);
     }
-    public void listsetdata(){
-        listPinCodeAdapter=new ListPinCodeAdapter(getActivity(),customerPinModel);
+    //---------------------------------------------------------------------------------------------- getAllPing
+
+
+    //---------------------------------------------------------------------------------------------- getDataPinRequest
+    private void getDataPinRequest(Query q) {
+        CustomerPinModelRepository pinModelRepository = new CustomerPinModelRepository();
+        customerPinModel = pinModelRepository.getItems(q);
+        listSetData();
+    }
+    //---------------------------------------------------------------------------------------------- getDataPinRequest
+
+
+    //---------------------------------------------------------------------------------------------- listSetData
+    public void listSetData() {
+        ListPinCodeAdapter listPinCodeAdapter = new ListPinCodeAdapter(getActivity(), customerPinModel);
         LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
-        list_request.setLayoutManager(llm);
-        list_request.setAdapter(listPinCodeAdapter);
+        recyclerViewPins.setLayoutManager(llm);
+        recyclerViewPins.setAdapter(listPinCodeAdapter);
     }
+    //---------------------------------------------------------------------------------------------- listSetData
 
-    public void restData(){
+
+    //---------------------------------------------------------------------------------------------- restData
+    public void restData() {
         final ProgressDialog progressDialog = new ProgressDialog(getContext());
         progressDialog.setMessage(getString(R.string.downloading_data));
         progressDialog.show();
-        DataManager dataManager=new DataManager(getContext());
+        DataManager dataManager = new DataManager(getContext());
         dataManager.getCustomerPin2(new DataManager.Callback() {
             @Override
             public void onSuccess() {
-                if (progressDialog != null && progressDialog.isShowing()) {
+                if (progressDialog.isShowing()) {
                     try {
-                        getDataPinRequest();
-                        listsetdata();
+                        editTextSearch.setText("");
+                        getAllPing();
                         progressDialog.dismiss();
                     } catch (Exception ignored) {
 
@@ -98,7 +156,7 @@ public class ListPinCode_Fragment extends IMainPageFragment {
             @Override
             public void onError(String error) {
                 showError(error);
-                if (progressDialog != null && progressDialog.isShowing()) {
+                if (progressDialog.isShowing()) {
                     try {
                         progressDialog.dismiss();
                     } catch (Exception ignored) {
@@ -108,7 +166,10 @@ public class ListPinCode_Fragment extends IMainPageFragment {
             }
         });
     }
+    //---------------------------------------------------------------------------------------------- restData
 
+
+    //---------------------------------------------------------------------------------------------- showError
     private void showError(String error) {
         Context context = getContext();
         if (isResumed() && context != null) {
@@ -120,4 +181,5 @@ public class ListPinCode_Fragment extends IMainPageFragment {
             dialog.show();
         }
     }
+    //---------------------------------------------------------------------------------------------- showError
 }
