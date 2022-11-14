8f1c62e0e992c8223a3246db3dacb4a1476f43ef
ZARMACARON\a-soleymani <alisoleymanizar1400@gmail.com>
2022-11-14 16:39:06 +0330

    راه اندازی api openweather

--- vaslibrary/src/main/java/com/varanegar/vaslibrary/webapi/supervisor/SupervisorApi.java
+++ vaslibrary/src/main/java/com/varanegar/vaslibrary/webapi/supervisor/SupervisorApi.java
@@ -58,11 +58,13 @@ public class SupervisorApi extends BaseApi implements ISupervisorApi {
 
     @Override
     public Call<List<OrderStatusReport>> OrderStatusReport(OrderStatusModel orderStatusModel) {
-        return getRetrofitBuilder(TokenType.UserToken, getBaseUrl()).build().create(ISupervisorApi.class).OrderStatusReport(orderStatusModel);
+        return getRetrofitBuilder(TokenType.UserToken, getBaseUrl()).build().create(ISupervisorApi.class)
+                .OrderStatusReport(orderStatusModel);
     }
     @Override
     public Call<List<ReturnDealerModel>> GetReturnReport(OrderStatusModel param) {
-        return getRetrofitBuilder(TokenType.UserToken, getBaseUrl()).build().create(ISupervisorApi.class).GetReturnReport(param);
+        return getRetrofitBuilder(TokenType.UserToken, getBaseUrl()).build()
+                .create(ISupervisorApi.class).GetReturnReport(param);
     }
 
 
