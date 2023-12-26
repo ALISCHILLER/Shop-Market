3e74db4607670bf6bdda07c6f2f58b34fa623566
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2023-12-26 09:41:12 +0330

    release/Dist-3.1.1.116

--- vaslibrary/src/main/java/com/varanegar/vaslibrary/manager/paymentmanager/PaymentManager.java
+++ vaslibrary/src/main/java/com/varanegar/vaslibrary/manager/paymentmanager/PaymentManager.java
@@ -899,17 +899,21 @@ public class PaymentManager extends BaseManager<PaymentModel> {
         if (cash != null)
             cashCheckReceiptModel.Cash = cash.Amount;
         if (discount != null)
-            cashCheckReceiptModel.Cash = cashCheckReceiptModel.Cash.add(discount.Amount);
+            cashCheckReceiptModel.Cash = cashCheckReceiptModel.Cash.add(discount.Amount)
+                    .setScale(2, BigDecimal.ROUND_HALF_DOWN);;
         if (credit != null)
-            cashCheckReceiptModel.Cash = cashCheckReceiptModel.Cash.add(credit.Amount);
+            cashCheckReceiptModel.Cash = cashCheckReceiptModel.Cash.add(credit.Amount)
+                    .setScale(2, BigDecimal.ROUND_HALF_DOWN);;
         List<PaymentModel> posModels = paymentManager.listPosPayments(customerId);
         for (PaymentModel paymentModel : posModels)
-            cashCheckReceiptModel.Cash = cashCheckReceiptModel.Cash.add(paymentModel.Amount);
+            cashCheckReceiptModel.Cash = cashCheckReceiptModel.Cash.add(paymentModel.Amount)
+                    .setScale(2, BigDecimal.ROUND_HALF_DOWN);;
 
         // calc check amounts
         List<PaymentModel> checkModels = paymentManager.listCheckPayments(customerId);
         for (PaymentModel paymentModel : checkModels)
-            cashCheckReceiptModel.Check = cashCheckReceiptModel.Check.add(paymentModel.Amount);
+            cashCheckReceiptModel.Check = cashCheckReceiptModel.Check.add(paymentModel.Amount)
+                    .setScale(2, BigDecimal.ROUND_HALF_DOWN);
         // calc receipt amount
         Currency receiptAmount = customerPayment.getTotalAmount(false).subtract(paymentManager.getTotalPaid(customerId));
         if (receiptAmount.compareTo(Currency.ZERO) == -1)
